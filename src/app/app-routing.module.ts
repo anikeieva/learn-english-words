3667a34bd0748e15ab437018e210fc90730f9a10
anikeieva <alyonaoooooooo@gmail.com>
2020-04-29 00:40:12 +0300

    Added video page to structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {FlashcardsComponent} from './flashcards/flashcards.component';
+import {VideosComponent} from './videos/videos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: FlashcardsComponent},
+  {path: 'flashcards', component: FlashcardsComponent},
+  {path: 'videos', component: VideosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
